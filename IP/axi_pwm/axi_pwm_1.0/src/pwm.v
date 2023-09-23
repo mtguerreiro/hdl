cab2400e7053b34b1d7a4b7400664b82c1c0fcd6
Marco Guerreiro <marcotulio.guerreiro@gmail.com>
2023-09-23 23:31:25 +0200

    Regenerated PWM IP

--- IP/axi_pwm/axi_pwm_1.0/src/pwm.v
+++ IP/axi_pwm/axi_pwm_1.0/src/pwm.v
@@ -49,128 +49,107 @@ output wire ovf_trigger;
 
 reg [31:0] period_int = 32'b0;
 reg [31:0] duty_int = 32'b0;
-reg[31:0] counter = 32'b0;
 reg [31:0] dead_time_int = 32'b0;
+
+reg [31:0] base_counter = 32'b0;
+reg [31:0] base_period = 32'b0;
+
+reg [31:0] pwm_counter = 32'b0;
 
 reg pwm_signal = 1'b0;
 reg pwm_cmp_signal = 1'b0;
 
-reg dir = 1'b0;
-
 reg ovf_signal = 1'b0;
 
-reg [31:0] counter_inc = 32'b1;
-
-/* Counter */
+/* Generates base counter and reload */
 always @ (posedge clk)
 begin
     if( reset == 1'b1 ) begin
-        counter <= 32'b0;
-    end
-    else begin
-        counter <= counter + counter_inc;
-    end
-end
-
-/* Set counter increment/decrement */
-always @(negedge clk)
-begin
-    if( reset == 1'b1 ) begin
-        counter_inc <= -1;
-    end
-    else begin
-        if( dir == 1'b1 ) begin
-            counter_inc <= -counter_inc;
-        end
-    end
-end
-
-/* Change counter direction and ovf signal generation */
-always @ (*)
-begin
-    if( reset == 1'b1 ) begin
-        dir = 1'b0;
-        ovf_signal = 1'b0;
-    end
-    else begin
-        if( counter >= period_int ) begin
-            dir = 1'b1;
-            ovf_signal = 1'b1;
-        end
-        else if( counter == 0) begin
-            dir = 1'b1;
-            ovf_signal = 1'b0;
-        end
-        else begin
-            ovf_signal = 1'b0;
-            dir = 1'b0;
-        end
-    end
-end
-
-/* Reload */
-always @(negedge clk)
-begin
-    if( reset == 1'b1 ) begin
-        period_int <= period;
+        base_counter <= 32'b0;        
         duty_int <= duty;
+        period_int <= period;
         dead_time_int <= dead_time;
+        
+        if( period == 0 ) base_period <= 0;
+        else base_period <= (period << 1) - 1;
     end
     else begin
-        if( (dir == 1'b1) && (counter_inc == -32'b1) ) begin
-            period_int <= period;
-            duty_int <= duty;
-            dead_time_int <= dead_time;
-        end
-    end
-end
-
-/* PWM generation */
-always @ (*)
-begin
-    if( reset == 1'b1 ) begin
-        pwm_signal = 1'b0;
-        pwm_cmp_signal = 1'b0;
-    end
-    else begin
-        if( counter_inc == 32'b1 ) begin
-            if( counter >= (duty_int - dead_time_int) ) begin
-                if( counter >= duty_int ) begin
-                    pwm_signal = 1'b0;
-                    pwm_cmp_signal = 1'b1;
-                end
-                else begin
-                    pwm_signal = 1'b0;
-                    pwm_cmp_signal = 1'b0;                
-                end
-            end
-            else begin
-                pwm_cmp_signal = 1'b0;
-                pwm_signal = 1'b1;
-            end
+        
+        if( base_counter < base_period ) begin
+            base_counter <= base_counter + 1'b1;
         end
         else begin
-            if( counter <= (duty_int + dead_time_int) ) begin
-                if( counter <= duty_int ) begin
-                    pwm_cmp_signal = 1'b0;
-                    pwm_signal = 1'b1;
-                end
-                else begin
-                    pwm_cmp_signal = 1'b0;
-                    pwm_signal = 1'b0;                
-                end
-            end
-            else begin
-                pwm_cmp_signal = 1'b1;
-                pwm_signal = 1'b0;
-            end        
+            base_counter <= 32'b0;
+            duty_int <= duty;
+            period_int <= period;
+            dead_time_int <= dead_time;
+
+            if( period == 0 ) base_period <= 0;
+            else base_period <= (period << 1) - 1;
         end
     end
 end
 
-///* PWM output */
+/* Generates overflow signal */
+always @ (posedge clk)
+begin
+    if( reset == 1'b1 ) begin
+        ovf_signal <= 1'b0;
+    end
+    else begin
+        if( base_counter == base_period ) ovf_signal <= 1'b1;
+        else ovf_signal <= 1'b0;
+    end
+end
+
+/* Generates PWM signals */
+always @ (posedge clk)
+begin
+    if( reset == 1'b1 ) begin
+        pwm_signal <= 1'b0;
+        pwm_cmp_signal <= 1'b0;
+    end
+    else begin
+    
+        if( duty_int >= period_int) begin
+            pwm_cmp_signal <= 1'b0;
+            if( base_counter >= dead_time_int) pwm_signal <= 1'b1;
+        end
+        else if( duty_int == 0 ) begin
+            pwm_signal <= 1'b0;
+            if( base_counter >= dead_time_int) pwm_cmp_signal <= 1'b1;        
+        end
+        else begin
+            if( base_counter < period_int ) begin
+                if( base_counter < duty_int ) begin
+                    pwm_signal <= 1'b1;
+                    pwm_cmp_signal <= 1'b0;
+                end
+                else begin
+                    pwm_signal <= 1'b0;
+                    if( base_counter >= (duty_int + dead_time_int) ) pwm_cmp_signal <= 1'b1;
+                end
+            end
+            else begin
+                if( (duty_int + dead_time_int) >= base_period ) begin
+                    pwm_cmp_signal <= 1'b0;
+                    if( base_counter > (base_period - duty_int) ) pwm_signal <= 1'b1;
+                end
+                else if( base_counter <= (base_period - duty_int - dead_time_int) ) begin
+                    pwm_signal <= 1'b0;
+                    pwm_cmp_signal <= 1'b1;
+                end
+                else begin
+                    pwm_cmp_signal <= 1'b0;
+                    if( base_counter > (base_period - duty_int) ) pwm_signal <= 1'b1;
+                end
+            end
+        end
+    end
+end
+
+assign ovf_trigger = ovf_signal & ovf_trigger_enable;
 assign pwm = pwm_signal & ~reset & pwm_enable;
 assign pwm_cmp = pwm_cmp_signal & ~reset & pwm_enable;
-assign ovf_trigger = ovf_signal & ovf_trigger_enable;
 
 endmodule
---
+++ IP/axi_pwm/axi_pwm_1.0/src/pwm_tb.v
@@ -0,0 +1,110 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/12/2023 11:51:46 AM
+// Design Name: 
+// Module Name: pwm_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pwm_tb(
+
+    );
+
+reg clk = 1'b0;
+reg reset = 1'b1;
+
+reg [31:0] period = 5;
+reg [31:0] duty = 1;
+
+reg pwm_enable = 1'b1;
+wire pwm;
+wire pwm_cmp;
+
+reg [31:0] dead_time = 0;
+
+reg ovf_trigger_enable = 1'b1;
+wire ovf_trigger;
+
+reg [63:0] counter_tb = 0;
+
+pwm UUT (
+    .clk(clk), 
+    .reset(reset), 
+    .period(period), 
+    .duty(duty), 
+    .pwm_enable(pwm_enable),
+    .pwm(pwm), 
+    .pwm_cmp(pwm_cmp),
+    .dead_time(dead_time),
+    .ovf_trigger_enable(ovf_trigger_enable),
+    .ovf_trigger(ovf_trigger)
+);
+
+
+always
+begin
+clk = 1'b1;
+#5; 
+
+clk = 1'b0;
+#5;
+end
+
+always @ (posedge clk)
+begin
+
+    if(counter_tb <= 20) begin
+        reset <= 1;
+        period <= 10;
+        duty <= 2;
+        dead_time <= 1;
+    end
+    
+    else if( (counter_tb > 20) && (counter_tb < 80) ) begin
+        reset <= 0;
+    end
+
+    else if( (counter_tb > 90) && (counter_tb < 93) ) begin
+        duty <= 3;
+    end
+    
+    else if( (counter_tb > 150) && (counter_tb < 153) ) begin
+        duty <= 0;
+    end
+
+    else if( (counter_tb > 210) && (counter_tb < 213) ) begin
+        duty <= 500;
+    end
+    
+    else if( (counter_tb > 260) && (counter_tb < 263) ) begin
+        duty <= 8;
+    end
+         
+    else if( (counter_tb > 350) && (counter_tb < 360) ) begin
+        ovf_trigger_enable <= 0;
+        pwm_enable <= 0;
+    end
+end
+
+
+always @ (posedge clk)
+begin
+counter_tb <= counter_tb + 1;
+end
+
+
+endmodule
